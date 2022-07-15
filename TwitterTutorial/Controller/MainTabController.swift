93723cb928b0beee480c9ef8fdc8dce42b71a4ff
V <V>
2022-07-15 23:42:15 +0400

    create mainTabBar

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by Apple on 15.07.2022.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+  
+
+}
